5d13c31af12fea2d2d77de7338a909bb65a0629b
AlexeyDubtsov <alexeydubtsov@gmail.com>
2023-02-23 00:55:19 +0300

    webhook in progress

--- Makefile
+++ Makefile
@@ -4,14 +4,20 @@ help:
 	@awk 'BEGIN {FS = ":.*##"; printf "Usage: make \033[36m<target>\033[0m\n"} /^[a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-10s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
 run:
-	docker-compose -f config/dev/docker-compose.yml up -d
+	docker-compose -f config/dev/polling.yml up -d
 
 stop:
-	docker-compose -f config/dev/docker-compose.yml down -v --remove-orphans
+	docker-compose -f config/dev/polling.yml down -v --remove-orphans
+
+run_webhook:
+	docker-compose -f config/dev/webhook.yml up -d
+
+stop_webhook:
+	docker-compose -f config/dev/polling.yml down -v --remove-orphans
 
 build:
 	docker image rm dev-bot dev-app dev-watcher
-	docker-compose -f config/dev/docker-compose.yml build
+	docker-compose -f config/dev/polling.yml build
 
 restart_bot:
 	docker stop dev-bot-1
--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,24 @@
+import argparse
 import asyncio
+import json
 import logging
 import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import BotCommand
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 
 from api.v1.google_calendar import register_google_handlers
 from middleware.user_access import UserAccessMiddleware
 
-bot = Bot(token=os.getenv("TG_BOT_TOKEN"))
-dp = Dispatcher()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--webhook', action=argparse.BooleanOptionalAction)
+args = parser.parse_args()
 
 
-@dp.message(Command(commands=["start"]))
 async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` command
@@ -32,14 +37,41 @@ async def set_commands(bot: Bot):
     await bot.set_my_commands(commands=commands)
 
 
-async def main(bot: Bot, dp: Dispatcher):
+async def polling_setup(bot: Bot, dp: Dispatcher):
     await bot.delete_webhook()
-    register_google_handlers(dp=dp)
-    dp.message.outer_middleware(UserAccessMiddleware())
     await set_commands(bot)
     await dp.start_polling(bot)
 
 
+async def webhook_setup(dispatcher: Dispatcher, bot: Bot):
+    await bot.delete_webhook()
+    await set_commands(bot)
+    # cert = FSInputFile("./cert/PUBLIC.pem")
+    await bot.set_webhook(f"https://194.67.109.17", ip_address="194.67.109.17")
+    info = await bot.get_webhook_info()
+    info = json.loads(info.json())
+    logging.info(f"webhook info:\n{json.dumps(info, indent=4)}")
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(main(bot, dp))
+    logging.basicConfig(format="%(asctime)s %(message)s",
+                        datefmt="%m/%d/%Y %I:%M:%S %p %Z",
+                        level=logging.INFO)
+
+    bot = Bot(token=os.getenv("TG_BOT_TOKEN"), parse_mode="HTML")
+    dp = Dispatcher()
+    dp.message.register(Command(commands=["start"]))
+    register_google_handlers(dp=dp)
+    dp.message.outer_middleware(UserAccessMiddleware())
+
+    webhook = args.webhook
+    if webhook:
+        logging.info("Starting with webhook")
+        dp.startup.register(webhook_setup)
+        app = web.Application()
+        SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="")
+        setup_application(app, dp, bot=bot)
+        web.run_app(app)
+    else:
+        logging.info("Starting with polling")
+        asyncio.run(polling_setup(bot=bot, dp=dp))
